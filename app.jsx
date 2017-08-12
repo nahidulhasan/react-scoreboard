8213faa562727a96163bd2b5778e2df1eb61efd3
nahid35 <nahidul.cse@gmail.com>
2017-08-12 21:28:59 +0600

    Developing scorebaord

--- app.jsx
+++ app.jsx
@@ -1,12 +1,41 @@
 function Application(){
 	return(
-	    <div>
-	    <h1> Hello world from React</h1>
-	    <p> Paragraph from React Application  </p>
+	    <div className="scoreboard">
+			 <div className="header">
+	    	   <h1> Score Board </h1>  
+	    	 </div>
+
+	    	<div className="players">
+	    	    <div className="player"> 
+		    	     <div className="player-name">
+	                   Nahidul Hasan
+		    	     </div>
+		    	    <div className="player-score">
+		    	         <div className="counter"> 
+		    	  	       <button className="counter-action-decrement"> - </button>
+		    	  	       <button className="counter-score"> 31 </button>
+		    	  	       <button className="counter-action-increment"> + </button>
+		    	  	       
+		    	  	    </div>
+		    	    </div> 
+
+		    	    <div className="player-name">
+	                   Mahmudul Hasan
+		    	     </div>
+		    	    <div className="player-score">
+		    	         <div className="counter"> 
+		    	  	       <button className="counter-action-decrement"> - </button>
+		    	  	       <button className="counter-score"> 30 </button>
+		    	  	       <button className="counter-action-increment"> + </button>
+		    	  	       
+		    	  	    </div>
+		    	    </div> 
+		    	  	  
+	    	    </div>
+	    	</div>
 	    </div>
 	);
 }
-
 
 ReactDOM.render(
   <Application/>,
